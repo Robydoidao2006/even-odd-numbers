528ccc5eaea77537ada2163cbdda0a984cdb988d
robert Fialho <32443950+Robydoidao2006@users.noreply.github.com>
2018-09-18 12:17:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+var number = document.querySelector('#number').value; // number to be divide 
+
+var button = document.querySelector('#buttonOne');
+button.addEventListener('click', function(){
+
+    var number            = document.querySelector('#number').value; // number to be divide 
+    var displayMessageOne = document.querySelector('#displayMessageOne');
+    var messageEven       = number + ' is an even number';
+    var messageOdd        = number + ' is an odd number';   
+
+    // checks a sigle user input and outputs a single result.
+    function eventOne(){
+   
+        if (number % 2 === 0){
+            displayMessageOne.textContent = messageEven;
+            displayMessageOne.style.background = 'green';
+        } else {
+            displayMessageOne.textContent = messageOdd;
+            displayMessageOne.style.background = 'red';
+        }
+
+    } 
+    eventOne();
+
+});
+
+var button = document.querySelector('#buttonTwo');
+button.addEventListener('click', function(){
+
+    var numberArray        = document.querySelector('#numberArray').value; // arra number
+    var displayMessageEven = document.querySelector('#displayMessageEven');
+    var displayMessageOdd  = document.querySelector('#displayMessageOdd');
+  
+    // loops throu array and display all even and odd numbers
+    function eventTwo(){
+        
+        for (var i = 0; i < numberArray; i++){   
+            
+            var messageEven =  i + ' is an even number' + '<br>';
+            var messageOdd  =  i + ' is an odd number' + '<br>';
+
+            if ( i % 2 === 0){
+
+                displayMessageEven.innerHTML += messageEven;
+                //displayMessageTwo.style.background += 'green'; 
+
+            } else {
+
+                displayMessageOdd.innerHTML += messageOdd ;
+                //displayMessageTwo.style.background = 'red';
+            }          
+        }                
+    }
+    eventTwo();
+}); // event listener.
+
+
+
+
